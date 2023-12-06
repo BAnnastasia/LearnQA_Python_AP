171015f424f524afa614466c014ceea94ec30c3c
Anastasia Borodich <>
2023-12-06 20:59:57 +0300

    2.Ex6_2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# from json.decoder import JSONDecodeError
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True,
                         headers={"User-Agent": "PostmanRuntime/7.33.0"})
 
-test_l = len(response.history)
+redirect_cnt = len(response.history)
 
-for var in range(0, test_l):
+for var in range(0, redirect_cnt):
     print(response.history[var].url)
 
-print(response.status_code)
+print(f"Количество редиректов: {redirect_cnt}")
+print(f"Конечный урл: {response.url}")
