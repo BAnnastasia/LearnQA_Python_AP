20cd92859eac3236b49a70f26a28ce04f59b2b51
Anastasia Borodich <>
2023-12-13 18:48:48 +0300

    3.Ex11_2

--- test_exercise11.py
+++ test_exercise11.py
@@ -2,10 +2,12 @@ import requests
 
 class TestExercise11:
     def test_check(self):
+        expected_parameter = "HomeWork"
+        expected_value = "hw_value"
         url = "https://playground.learnqa.ru/api/homework_cookie"
         request1 = requests.get(url)
         cookies_dict = dict(request1.cookies)
         print(cookies_dict)
         assert request1.status_code == 200, "Wrong response code"
-        assert "HomeWork" in cookies_dict, "There is not 'HomeWork' in the cookies"
-        assert cookies_dict['HomeWork'] == "hw_value", "There is not 'hw_value' value in the 'HomeWork' parameter"
+        assert expected_parameter in cookies_dict, f"There is not '{expected_parameter}' in the cookies"
+        assert cookies_dict[expected_parameter] == expected_value, f"There is not '{expected_value}' value in the '{expected_parameter}' parameter"
